3a47250bc5d669fb7c6a93776db2f809b84fac1e
Sugar Teddy <cashteddy@protonmail.com>
2021-08-23 15:17:38 +0200

    fix subgraph.yaml by adding avalanche.json stub

--- packages/subgraph/subgraph.yaml.js
+++ packages/subgraph/subgraph.yaml.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const network = process.argv[2] || "mainnet";
+const network = process.argv[2] || "avalanche";
 const { addresses, startBlock } = require(`@liquity/lib-ethers/deployments/${network}.json`);
 
 console.log(`Preparing subgraph manifest for network "${network}"`);
