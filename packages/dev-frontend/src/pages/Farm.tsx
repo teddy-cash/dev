879ba515cf2600f2b3bd3947756234b86e2b23b7
Mr.Hello <mr.hello4evar@gmail.com>
2021-09-07 09:45:27 -0400

    Comment out unused imports

--- packages/dev-frontend/src/pages/Farm.tsx
+++ packages/dev-frontend/src/pages/Farm.tsx
@@ -1,8 +1,8 @@
 import { Container } from "theme-ui";
 import { SystemStats } from "../components/SystemStats";
-import { Farm as FarmPanel } from "../components/Farm/Farm";
+//import { Farm as FarmPanel } from "../components/Farm/Farm";
 import { Farm as FarmTjPanel } from "../components/FarmTj/Farm";
-import { Farm as FarmPngPanel } from "../components/FarmPng/Farm";
+//import { Farm as FarmPngPanel } from "../components/FarmPng/Farm";
 
 export const Farm: React.FC = () => (
   <Container variant="columns" sx={{ justifyContent: "flex-start" }}>
