74f48d9e3228962493d5fe0fddd16b33ba77597f
Mr.Hello <mr.hello4evar@gmail.com>
2021-09-10 09:08:15 -0400

    Fix time lock on farm

--- packages/dev-frontend/src/components/FarmPng/views/Disabled/Disabled.tsx
+++ packages/dev-frontend/src/components/FarmPng/views/Disabled/Disabled.tsx
@@ -32,7 +32,7 @@ export const Disabled: React.FC = () => {
       console.log(`Farm already unlocked`);
       return ;
     }
-  }, [])
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   const { pngLiquidityMiningStake: liquidityMiningStake, pngLiquidityMiningLQTYReward: liquidityMiningLQTYReward } = useLiquitySelector(selector);
   
