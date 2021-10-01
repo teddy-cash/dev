21f9f8865a3ac567d02e0a8f1696e5e2af2a07c8
Sugar Teddy <cashteddy@protonmail.com>
2021-10-01 14:35:32 +0200

    change separators.

--- packages/dev-frontend/src/components/Footer.tsx
+++ packages/dev-frontend/src/components/Footer.tsx
@@ -8,15 +8,15 @@ export const Footer = () => {
         <Box sx={{mt: 4, fontSize:1, fontWeight: "normal", display: "flex"}}>
             <Text sx={textStyle}>
               <Link href="https://teddy.cash" target="_blank">Website</Link>
-              , 
+              &nbsp;&middot;&nbsp; 
               <Link href="https://docs.teddy.cash" target="_blank">Docs</Link>
-            ,  <Link href="https://twitter.com/TeddyCashLive" target="_blank">Twitter</Link>
-            ,  <Link href="https://medium.com/@teddy.cash" target="_blank">Medium</Link>
-          ,   <Link href="https://github.com/teddy-cash" target="_blank">Github</Link>
+            &nbsp;&middot;&nbsp;  <Link href="https://twitter.com/TeddyCashLive" target="_blank">Twitter</Link>
+            &nbsp;&middot;&nbsp;  <Link href="https://medium.com/@teddy.cash" target="_blank">Medium</Link>
+          &nbsp;&middot;&nbsp;   <Link href="https://github.com/teddy-cash" target="_blank">Github</Link>
             
-           ,   <Link href="https://discord.gg/TJXnyPXQxf" target="_blank">Discord</Link>
-            ,  <Link href="https://t.me/teddycashofficial" target="_blank">Telegram</Link>
-            ,  <Link href="https://defillama.com/protocol/teddy-cash" target="_blank">DeFiLlama</Link>
+           &nbsp;&middot;&nbsp;   <Link href="https://discord.gg/TJXnyPXQxf" target="_blank">Discord</Link>
+            &nbsp;&middot;&nbsp;  <Link href="https://t.me/teddycashofficial" target="_blank">Telegram</Link>
+            &nbsp;&middot;&nbsp;  <Link href="https://defillama.com/protocol/teddy-cash" target="_blank">DeFiLlama</Link>
             </Text>
             
         </Box>
