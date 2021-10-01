4eca452f6141077b9b53ad64bef88be230830480
Sugar Teddy <cashteddy@protonmail.com>
2021-10-01 14:33:57 +0200

    fixing footer wrecking layout

--- packages/dev-frontend/src/components/Footer.tsx
+++ packages/dev-frontend/src/components/Footer.tsx
@@ -5,30 +5,18 @@ export const Footer = () => {
     const textStyle = {ml: 4, fontWeight: 'bold'};
     return (
     <div>
-        <Box sx={{mt: 4, fontSize:3, display: "flex"}}>
+        <Box sx={{mt: 4, fontSize:1, fontWeight: "normal", display: "flex"}}>
             <Text sx={textStyle}>
               <Link href="https://teddy.cash" target="_blank">Website</Link>
-            </Text>
-            <Text sx={textStyle}>
+              , 
               <Link href="https://docs.teddy.cash" target="_blank">Docs</Link>
-            </Text>
-            <Text sx={textStyle}>
-              <Link href="https://twitter.com/TeddyCashLive" target="_blank">Twitter</Link>
-            </Text>
-            <Text sx={textStyle}>
-              <Link href="https://medium.com/@teddy.cash" target="_blank">Medium</Link>
-            </Text>
-            <Text sx={textStyle}>
-              <Link href="https://github.com/teddy-cash" target="_blank">Github</Link>
-            </Text>
-            <Text sx={textStyle}>
-              <Link href="https://discord.gg/TJXnyPXQxf" target="_blank">Discord</Link>
-            </Text>
-            <Text sx={textStyle}>
-              <Link href="https://t.me/teddycashofficial" target="_blank">Telegram</Link>
-            </Text>
-            <Text sx={textStyle}>
-              <Link href="https://defillama.com/protocol/teddy-cash" target="_blank">DeFi Llama</Link>
+            ,  <Link href="https://twitter.com/TeddyCashLive" target="_blank">Twitter</Link>
+            ,  <Link href="https://medium.com/@teddy.cash" target="_blank">Medium</Link>
+          ,   <Link href="https://github.com/teddy-cash" target="_blank">Github</Link>
+            
+           ,   <Link href="https://discord.gg/TJXnyPXQxf" target="_blank">Discord</Link>
+            ,  <Link href="https://t.me/teddycashofficial" target="_blank">Telegram</Link>
+            ,  <Link href="https://defillama.com/protocol/teddy-cash" target="_blank">DeFiLlama</Link>
             </Text>
             
         </Box>
