8e1babc2b3240984543c088c4de8213e1d45ff9c
Mr.Hello <mr.hello4evar@gmail.com>
2021-10-11 20:13:08 -0400

    Add farm details

--- packages/dev-frontend/src/pages/Farm.tsx
+++ packages/dev-frontend/src/pages/Farm.tsx
@@ -1,5 +1,6 @@
 import { Container } from "theme-ui";
 import { SystemStats } from "../components/SystemStats";
+import { FarmsEnded } from "../components/FarmsEnded";
 import { Farm as FarmPanel } from "../components/Farm/Farm";
 //import { Farm as FarmTjPanel } from "../components/FarmTj/Farm";
 import { Farm as FarmPngPanel } from "../components/FarmPng/Farm";
@@ -7,6 +8,7 @@ import { Farm as FarmPngPanel } from "../components/FarmPng/Farm";
 export const Farm: React.FC = () => (
   <Container variant="columns" sx={{ justifyContent: "flex-start" }}>
     <Container variant="left">
+      <FarmsEnded />
       <FarmPanel />
       {/* <FarmTjPanel /> */}
       <FarmPngPanel />
