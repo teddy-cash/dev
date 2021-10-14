45d54fa0653b99c693cec1b4cd795f0ffe2aa8e0
Mr.Hello <mr.hello4evar@gmail.com>
2021-10-14 10:37:56 -0400

    Add link to ended farms

--- packages/dev-frontend/src/components/FarmsEnded.tsx
+++ packages/dev-frontend/src/components/FarmsEnded.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Card, Heading, Box, Flex, Image, Link } from "theme-ui";
 import { Icon } from "./Icon";
 import { useLiquity } from "../hooks/LiquityContext";
-
+import { NavLink } from "react-router-dom";
 const TJ_POOlS = {
   'TSD': '0x2d16af2d7f1edb4bc5dbadf3fff04670b4bcd0bb',
   'TEDDY': '0x91f0963873bbca2e58d21bb0941c0d859db3ca31'
@@ -27,7 +27,9 @@ export const FarmsEnded: React.FC = () => {
       </Heading>
       <Box sx={{ p: [2, 3] }}>
         <Box>
-          <Flex style={{marginBottom: 5}}>Our Liquidity Farms have ended!<br />Please move your liquidity to these excellent farms</Flex>
+          <Box style={{marginBottom: 10}}>Our Liquidity Farms for AVAX/TSD and AVAX/TEDDY have ended!
+          They can be accessed <NavLink exact to="/ended-farms" style={{textDecoration: 'none'}}>here</NavLink><br /><br />
+          Please move your liquidity to these excellent farms</Box>
           <Flex sx={{paddingBottom: 2, marginBottom: 20, borderBottom: 1, borderColor: "rgba(0, 0, 0, 0.1)"}}>
             <Image src="./pangolin.svg" width="20" height="20" sx={{marginRight: 2}}/>Pangolin<br />
           </Flex>
--- packages/dev-frontend/src/pages/DisabledFarms.tsx
+++ packages/dev-frontend/src/pages/DisabledFarms.tsx
@@ -1,11 +1,14 @@
 import { Container } from "theme-ui";
 import { SystemStats } from "../components/SystemStats";
-import { FarmsEnded } from "../components/FarmsEnded";
+import { Farm as FarmPanel } from "../components/Farm/Farm";
+//import { Farm as FarmTjPanel } from "../components/FarmTj/Farm";
+import { Farm as FarmPngPanel } from "../components/FarmPng/Farm";
 
-export const Farm: React.FC = () => (
+export const DisabledFarms: React.FC = () => (
   <Container variant="columns" sx={{ justifyContent: "flex-start" }}>
     <Container variant="left">
-      <FarmsEnded />
+      <FarmPanel />
+      <FarmPngPanel />
     </Container>
 
     <Container variant="right">
