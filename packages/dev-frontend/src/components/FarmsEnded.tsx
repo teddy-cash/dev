642911eede701d497ad039e3bf2d6c66eee9d9c8
Mr.Hello <mr.hello4evar@gmail.com>
2021-10-14 10:53:42 -0400

    Add link to Snowball

--- packages/dev-frontend/src/components/FarmsEnded.tsx
+++ packages/dev-frontend/src/components/FarmsEnded.tsx
@@ -8,7 +8,6 @@ const TJ_POOlS = {
   'TEDDY': '0x91f0963873bbca2e58d21bb0941c0d859db3ca31'
 };
 
-
 export const FarmsEnded: React.FC = () => {
     const {
     liquity: {
@@ -33,18 +32,18 @@ export const FarmsEnded: React.FC = () => {
           <Flex sx={{paddingBottom: 2, marginBottom: 20, borderBottom: 1, borderColor: "rgba(0, 0, 0, 0.1)"}}>
             <Image src="./pangolin.svg" width="20" height="20" sx={{marginRight: 2}}/>Pangolin<br />
           </Flex>
-          <Flex style={{marginBottom: 15}}>
+          <Flex style={{marginBottom: 15, alignItems: 'center'}}>
             <Image src="./icons/avalanche-avax-logo.svg" width="20" height="20" sx={{marginRight: 1}}/>
-            <Image src="./tsd.png" width="25" height="25" sx={{marginRight: 1}}/>
+            <Image src="./tsd.png" width="25" height="20" sx={{marginRight: 1}}/>
 
             AVAX-TSD
             <Link href={`https://app.pangolin.exchange/#/png/AVAX/${addresses['lusdToken']}/1`} target="_blank">
-              <Icon name="external-link-alt" style={{marginLeft: 10, marginTop: 5}}/>
+              <Icon name="external-link-alt" style={{marginLeft: 10, marginTop: 5, marginRight: 5}}/>
             </Link>
           </Flex>
           <Flex>
             <Image src="./icons/avalanche-avax-logo.svg" width="20" height="20" sx={{marginRight: 1}}/>
-            <Image src="./teddy-cash-icon.png" width="25" height="25" sx={{marginRight: 1}}/>
+            <Image src="./teddy-cash-icon.png" width="25" height="20" sx={{marginRight: 1}}/>
             AVAX-TEDDY
             <Link href={`https://app.pangolin.exchange/#/png/AVAX/${addresses['lqtyToken']}/1`} target="_blank">
               <Icon name="external-link-alt" style={{marginLeft: 10, marginTop: 5}}/>
@@ -71,7 +70,7 @@ export const FarmsEnded: React.FC = () => {
             </Link>
           </Flex>
           <Box style={{marginTop: 10}}>
-            <Link href="https://www.cycle.finance/" target="_blank">Cycle Finance</Link> provides auto-compounding strategies for the farms on Trader Joe
+            <Link href="https://app.snowball.network/compound-and-earn">Snowball</Link> and <Link href="https://www.cycle.finance/" target="_blank">Cycle Finance</Link> provide auto-compounding for select pools
           </Box>
         </Box>
         <Flex variant="layout.actions">
