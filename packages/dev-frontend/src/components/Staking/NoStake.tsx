6e50f4fe8c8e361b6464806118c2cf273c856804
Sugar Teddy <cashteddy@protonmail.com>
2021-10-27 18:36:13 +0200

    better buttons

--- packages/dev-frontend/src/components/Staking/NoStake.tsx
+++ packages/dev-frontend/src/components/Staking/NoStake.tsx
@@ -13,13 +13,17 @@ export const NoStake: React.FC = () => {
     <Card>
       <Heading><p>Staking</p></Heading>
       <Box sx={{ p: [2, 3] }}>
-        <InfoMessage title={`You haven't staked ${GT} yet.`}>
-          Stake {GT} to earn a share of borrowing and redemption fees. <Link href="https://docs.teddy.cash/teddy-staking#how-does-staking-work-in-liquity" target="_blank">How does staking work? <Icon name="external-link-alt" /></Link>
-        </InfoMessage>
-
-        <Flex variant="layout.actions">
-          <Button onClick={() => dispatch({ type: "startAdjusting" })}>Start staking</Button>
-        </Flex>
+        
+        Earn your share from borrowing and redemption fees. 
+        <br />
+        <Link href="https://docs.teddy.cash/teddy-staking#how-does-staking-work-in-liquity" target="_blank">How it works? <Icon name="external-link-alt" /></Link>
+        <br />
+        
+          <Button style={{display: "block", margin: "18px auto", lineHeight: 1.2}} onClick={() => dispatch({ type: "startAdjusting" })}>
+            <strong style={{fontSize: 18}}>Stake TEDDY</strong>
+            <br />
+            <small style={{fontWeight: "normal"}}>Earn TSD and AVAX</small>
+          </Button>
       </Box>
     </Card>
   );
