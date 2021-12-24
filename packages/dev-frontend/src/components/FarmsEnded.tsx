fb5d6bdd2211a0859ed30e865a8791fe821f7bd6
sugarteddy <88325029+sugarteddy@users.noreply.github.com>
2021-12-24 15:56:37 +0700

    updated farms page (#60)

--- packages/dev-frontend/src/components/FarmsEnded.tsx
+++ packages/dev-frontend/src/components/FarmsEnded.tsx
@@ -16,68 +16,117 @@ export const FarmsEnded: React.FC = () => {
   } = useLiquity();
   
   return  (
+    <>
     <Card>
       <Heading>
-        <Flex><Image src="./icons/avalanche-avax-logo.svg" width="20" height="20" sx={{marginRight: 1}}/>
-        <Image src="./tsd.png" width="25" height="25" sx={{marginRight: 1}}/>
-        <Image src="./teddy-cash-icon.png" width="25" height="25" sx={{marginRight: 2}}/>
-        Liquidity Farms
+        <Flex>
+        <Image src="./axial-144x144.png" width="25" height="25" sx={{marginRight: 2}}/>
+        Axial Exchange
         </Flex>
       </Heading>
+
       <Box sx={{ p: [2, 3] }}>
         <Box>
-          <Box style={{marginBottom: 10}}>Our Liquidity Farms for AVAX/TSD and AVAX/TEDDY have ended!
-          They can be accessed <NavLink exact to="/ended-farms" style={{textDecoration: 'none'}}>here</NavLink><br /><br />
-          Please move your liquidity to these excellent farms</Box>
-          <Flex sx={{paddingBottom: 2, marginBottom: 20, borderBottom: 1, borderColor: "rgba(0, 0, 0, 0.1)"}}>
-            <Image src="./pangolin.svg" width="20" height="20" sx={{marginRight: 2}}/>Pangolin<br />
-          </Flex>
-          <Flex style={{marginBottom: 15, alignItems: 'center'}}>
-            <Image src="./icons/avalanche-avax-logo.svg" width="20" height="20" sx={{marginRight: 1}}/>
+          <Flex style={{marginBottom: 15, alignItems: 'center', fontWeight: "bold", color: "white"}}>
             <Image src="./tsd.png" width="25" height="20" sx={{marginRight: 1}}/>
+            <Link href={`https://app.axial.exchange/#/pools/ac4d/deposit`} target="_blank">
+              A4CD Stablecoin Pool (Dual Rewards)
+              <Icon name="external-link-alt" style={{marginLeft: 10, marginTop: 5, marginRight: 5}}/>
+            </Link>
+          </Flex>
+        </Box>
+      </Box>
+    </Card>
+    <Card>
+      <Heading>
+        <Flex>
+        <Image src="./pangolin.svg" width="20" height="20" sx={{marginRight: 2}}/>
+          Pangolin
+        </Flex>
+      </Heading>
 
-            AVAX-TSD
+      <Box sx={{ p: [2, 3] }}>
+        <Box>
+          <Flex style={{marginBottom: 15, alignItems: 'center'}}>
+            <Image src="./tsd.png" width="25" height="20" sx={{marginRight: 1}}/>
+            AVAX-TSD (LP)
             <Link href={`https://app.pangolin.exchange/#/png/AVAX/${addresses['lusdToken']}/1`} target="_blank">
               <Icon name="external-link-alt" style={{marginLeft: 10, marginTop: 5, marginRight: 5}}/>
             </Link>
           </Flex>
-          <Flex>
-            <Image src="./icons/avalanche-avax-logo.svg" width="20" height="20" sx={{marginRight: 1}}/>
+
+          <Flex sx={{color: "white"}}>
             <Image src="./teddy-cash-icon.png" width="25" height="20" sx={{marginRight: 1}}/>
-            AVAX-TEDDY
             <Link href={`https://app.pangolin.exchange/#/png/AVAX/${addresses['lqtyToken']}/1`} target="_blank">
+              AVAX-TEDDY Farm (2x) 
               <Icon name="external-link-alt" style={{marginLeft: 10, marginTop: 5}}/>
             </Link>
+
           </Flex>
-          <Flex sx={{marginBottom: 10, marginTop: 30, paddingBottom: 2, borderBottom: 1, borderColor: "rgba(0, 0, 0, 0.1)"}}>
-            <Image src="./joe.png" width="25" height="25" sx={{marginRight: 2}}/>
-            Trader Joe
-          </Flex>
+        </Box>
+      </Box>
+    </Card>
+
+    <Card>
+      <Heading>
+        <Flex>
+        <Image src="./joe.png" width="25" height="25" sx={{marginRight: 2}}/>
+          Traderjoe
+        </Flex>
+      </Heading>
+      <Box sx={{ p: [2, 3] }}>
+        <Box>
           <Flex style={{marginBottom: 15}}>
-            <Image src="./icons/avalanche-avax-logo.svg" width="20" height="20" sx={{marginRight: 1}}/>
             <Image src="./tsd.png" width="25" height="25" sx={{marginRight: 1}}/>
-            AVAX-TSD
+            AVAX-TSD LP
             <Link href={`https://www.traderjoexyz.com/#/farm/${TJ_POOlS['TSD']}`} target="_blank">
               <Icon name="external-link-alt" style={{marginLeft: 10, marginTop: 5}}/>
             </Link>
           </Flex>
           <Flex>
-            <Image src="./icons/avalanche-avax-logo.svg" width="20" height="20" sx={{marginRight: 1}}/>
             <Image src="./teddy-cash-icon.png" width="25" height="25" sx={{marginRight: 1}}/>
-            AVAX-TEDDY
+            AVAX-TEDDY LP
             <Link href={`https://www.traderjoexyz.com/#/farm/${TJ_POOlS['TEDDY']}`} target="_blank">
               <Icon name="external-link-alt" style={{marginLeft: 10, marginTop: 5}}/>
             </Link>
-          </Flex>
+          </Flex> 
+        </Box>
+      </Box>
+    </Card>
+
+    <Card>
+      <Heading>
+        <Flex>
+          Auto Compounders
+        </Flex>
+      </Heading>
+      <Box sx={{ p: [2, 3] }}>
+        <Box>
           <Box style={{marginTop: 10}}>
             <Link href="https://app.snowball.network/compound-and-earn" target="_blank">Snowball</Link>, <Link href="https://yieldyak.com/farms" target="_blank">Yield Yak</Link>, and <Link href="https://www.cycle.finance/" target="_blank">Cycle Finance</Link> provide auto-compounding for select pools
+          </Box>
+        <Flex variant="layout.actions">
+        </Flex>
+      </Box>
+      </Box>
+    </Card>
+
+    <Card>
+      <Heading>
+        <Flex>
+          Other integrations
+        </Flex>
+      </Heading>
+      <Box sx={{ p: [2, 3] }}>
+        <Box>
+          <Box style={{marginTop: 10}}>
+            Transfer TSD privately with <Link href="https://app.sherpa.cash/" target="_blank">Sherpa Cash</Link>.
           </Box>
         </Box>
         <Flex variant="layout.actions">
         </Flex>
       </Box>
     </Card>
+    </>
   );
-
-
 };
